3550375344f25eec468496062bd620d2c57aae89
tirumala kumar <kumarktk4169@gmail.com>
2023-05-08 14:13:29 +0530

    added two classes
    provided methods

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+
+    int x=10;
+    int y=20;
+    int z=0;
+
+    public  void Adder(){
+        z=x+y;
+        System.out.println(z);
+    }
+}
---
+++ sub.java
@@ -0,0 +1,13 @@
+public class sub {
+
+    int x=15;
+    int y=5;
+
+    void sub(){
+        System.out.println(x-y);
+    }
+
+    void mul(){
+        System.out.println(x*y);
+    }
+}
